d1983a732d5514fb9877889833ae47732d786e82
Brielle  Natzic <briellenatzic@Brielles-MacBook-Air.local>
2025-10-15 10:54:58 -0400

    Added project header and dataset

---
+++ project1_penguins.py
@@ -0,0 +1,7 @@
+# Name: Brielle Natzic
+# Student ID: 9322 2332
+# Email: bnatzic@umich.edu
+# Collaborators: None
+# GenAI used: ChatGPT (used for guidance and code scaffolding; all code was reviewed, tested, and modified by me)
+# Function authorship: All functions written, tested, and verified by Brielle Natzic
+
